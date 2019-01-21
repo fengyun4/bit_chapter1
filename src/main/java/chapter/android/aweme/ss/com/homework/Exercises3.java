3439a21e1b040dc5763ef5fc9940c4ce7b4345e0
fengyun4 <2698303386@qq.com>
2019-01-21 11:31:21 +0800

    msg

---
+++ src/main/java/chapter/android/aweme/ss/com/homework/Exercises3.java
@@ -0,0 +1,55 @@
+package chapter.android.aweme.ss.com.homework;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.util.List;
+
+import chapter.android.aweme.ss.com.homework.model.Message;
+import chapter.android.aweme.ss.com.homework.model.PullParser;
+
+/**
+ * 大作业:实现一个抖音消息页面,所需资源已放在res下面
+ */
+public class Exercises3 extends AppCompatActivity implements MyAdapter.ListItemClickListener {
+
+    private static final String TAG = "This is the  Exercise_third";
+
+    private MyAdapter myAdapter;
+    public List<Message>messages;
+
+    private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips);
+
+        try {
+            InputStream assetInput = getAssets().open("data.xml");
+            messages = PullParser.pull2xml(assetInput);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        recyclerView=findViewById(R.id.rv_list);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        myAdapter=new MyAdapter(messages, this);
+
+        recyclerView.setAdapter(myAdapter);
+
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+
+    }
+}
---
+++ src/main/java/chapter/android/aweme/ss/com/homework/MyAdapter.java
@@ -0,0 +1,174 @@
+package chapter.android.aweme.ss.com.homework;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import chapter.android.aweme.ss.com.homework.model.Message;
+import chapter.android.aweme.ss.com.homework.widget.CircleImageView;
+
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.NumberViewHolder> {
+    private static final String TAG = "MyAdapter";
+    private int mNumberItems;
+    private final ListItemClickListener mONClickListener;
+
+    private List<Message> mdata;
+
+    public MyAdapter(List<Message> data, ListItemClickListener Listener){
+        mdata = data;
+        mONClickListener=Listener;
+    }
+
+
+    @NonNull
+    @Override
+    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context=viewGroup.getContext();
+        int layoutIdForListItem=R.layout.number_list_item;
+        LayoutInflater inflater=LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately=false;
+        View view=inflater.inflate(layoutIdForListItem,viewGroup,shouldAttachToParentImmediately);
+        NumberViewHolder viewHolder=new NumberViewHolder(view);
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.NumberViewHolder numViewHolder, int i) {
+        Log.d(TAG,"onBindViewHolder: #"+i);
+        numViewHolder.bind(i);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mdata.size();
+    }
+
+
+    public class NumberViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        //private final TextView viewHolderIndex;
+        //private final TextView listItemNumberView;
+
+        private TextView my_title;
+        private TextView my_words;
+        private TextView my_time;
+        private CircleImageView my_image;
+
+        public NumberViewHolder(@NonNull View itemView){
+            super(itemView);
+//           listItemNumberView=(TextView)itemView.findViewById(R.id.tv_item_number);
+//           viewHolderIndex=(TextView)itemView.findViewById(R.id.tv_view_holder_instance);
+//            itemView.setOnClickListener(this);
+            my_title=(TextView)itemView.findViewById(R.id.tv_title);
+            my_words=(TextView)itemView.findViewById(R.id.tv_description);
+            my_time=(TextView)itemView.findViewById(R.id.tv_time);
+            my_image=(CircleImageView)itemView.findViewById(R.id.iv_avatar);
+        }
+
+        public void bind(int position){
+            //listItemNumberView.setText(String.valueOf(position));
+
+            my_title.setText(mdata.get(position).getTitle());
+            my_words.setText(mdata.get(position).getDescription());
+            my_time.setText(mdata.get(position).getTime());
+
+
+            if(mdata.get(position).getIcon().equals("TYPE_ROBOT")){
+                my_image.setImageResource(R.drawable.session_robot);
+            }
+            else if(mdata.get(position).getIcon().equals("TYPE_SYSTEM")){
+                my_image.setImageResource(R.drawable.icon_blacksend_touch);
+            }
+            else if(mdata.get(position).getIcon().equals("TYPE_GAME")){
+                my_image.setImageResource(R.drawable.icon_micro_game_comment);
+            }
+            else if(mdata.get(position).getIcon().equals("TYPE_STRANGER")){
+                my_image.setImageResource(R.drawable.session_stranger);
+            }
+            else if(mdata.get(position).getIcon().equals("TYPE_USER")){
+                my_image.setImageResource(R.drawable.icon_girl);
+            }
+            else if(mdata.get(position).getIcon().equals("TYPE_OFFICIAL")){
+                my_image.setImageResource(R.drawable.im_icon_notice_official);
+            }
+        }
+        @Override
+        public void onClick(View v){
+            int clickedPosition =getAdapterPosition();
+            if(mONClickListener!=null){
+                mONClickListener.onListItemClick(clickedPosition);
+            }
+        }
+    }
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    //    @NonNull
+//    @Override
+//    public NumberViewHolder onCreteViewHolder(@NonNull ViewGroup viewGroup,int viewType){
+//        Context context=viewGroup.getContext();
+//        int layoutIdForListItem=R.layout.number_list_item;
+//        LayoutInflater inflater=LayoutInflater.from(context);
+//        boolean shouldAttachToParentImmediately=false;
+//
+//        View view=inflater.inflate(layoutIdForListItem,viewGroup,shouldAttachToParentImmediately);
+//        NumberViewHolder viewHolder=new NumberViewHolder(view);
+//
+//        return viewHolder;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull NumberViewHolder numberViewHolder,int position){
+//        Log.d(TAG,"onBindViewHolder:#"+position);
+//        numberViewHolder.bind(position);
+//    }
+//
+//
+//
+//    @Override
+//    public int getItemCount(){
+//        return mNumberItems;
+//    }
+
+//    public class NumberViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+//        private final TextView viewHolderIndex;
+//        private final TextView listItemNumberView;
+//
+//        public NumberViewHolder(@NonNull View itemView){
+//            super(itemView);
+//            listItemNumberView=(TextView)itemView.findViewById(R.id.tv_item_number);
+//            viewHolderIndex=(TextView)itemView.findViewById(R.id.tv_view_holder_instance);
+//            itemView.setOnClickListener(this);
+//        }
+//        public void bind(int position){
+//
+//            listItemNumberView.setText(String.valueOf(position));
+//        }
+//
+//        @Override
+//        public void onClick(View v){
+//            int clickedPosition=getAdapterPosition();
+//            if(mONClickListener!=null){
+//                mONClickListener.onListItemClick(clickedPosition);
+//            }
+//        }
+//    }
+//    public interface ListItemClickListener{
+//        void onListItemClick(int clickedItemIndex);
+//    }
+
+
+}
